777b6452fcafe4fb07963fccf48b09e172f92891
alvito <muhammadalvito23@gmail.com>
2024-12-30 10:00:18 +0700

    feat: add category get detail, update and delete functionality

--- src/controllers/category.controller.js
+++ src/controllers/category.controller.js
@@ -20,6 +20,33 @@ class CategoryController {
 
     ApiResponse.success(res, 200, "success", "Categories fetched successfully", data);
   }
+
+  static async getCategory(req, res) {
+    const { name } = req.params;
+
+    const data = await CategoryService.getCategory(name);
+
+    ApiResponse.success(res, 200, "success", "Category fetched successfully", data);
+  }
+
+  static async updateCategory(req, res) {
+    const payload = req.body;
+    const { name } = req.params;
+
+    CategorySchema.update().parse(payload);
+
+    const data = await CategoryService.updateCategory(name, payload);
+
+    ApiResponse.success(res, 200, "success", "Category updated successfully", data);
+  }
+
+  static async deleteCategory(req, res) {
+    const { name } = req.params;
+
+    const data = await CategoryService.deleteCategory(name);
+
+    ApiResponse.success(res, 200, "success", "Category deleted successfully", data);
+  }
 }
 
 module.exports = CategoryController;
--- src/routes/category.routes.js
+++ src/routes/category.routes.js
@@ -8,5 +8,10 @@ const router = Router();
 router.post("/", AuthMiddleware.isAuthenticated, AsyncHandler.wrap(CategoryController.createCategory));
 
 router.get("/", AsyncHandler.wrap(CategoryController.getCategories));
+router.get("/:name", AsyncHandler.wrap(CategoryController.getCategory));
+
+router.put("/:name", AuthMiddleware.isAuthenticated, AsyncHandler.wrap(CategoryController.updateCategory));
+
+router.delete("/:name", AuthMiddleware.isAuthenticated, AsyncHandler.wrap(CategoryController.deleteCategory));
 
 module.exports = router;
